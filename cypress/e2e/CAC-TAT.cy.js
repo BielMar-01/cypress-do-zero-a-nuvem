22c548073acfab25dfb4b9a320c3ce181950dbad
Gabriel Marques <bielmarques922@gmail.com>
2025-08-13 17:50:11 -0300

    :test_tube: test: Criando teste do Exercicio 1 da aula 2

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,6 +1,19 @@
 describe('Central de Atendimento ao Cliente TAT', () => {
+  beforeEach(() => {
+        cy.visit('./src/index.html') // visita o arquivo HTML local
+      })
+
   it('verifica o título da aplicação', () => {
-      cy.visit('./src/index.html') // visita o arquivo HTML local
-      cy.title().should('eq', 'Central de Atendimento ao Cliente TAT') // verifica o título
+      cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT') // verifica o título
+  })
+
+  it.only('preenche os campos obrigatórios e envia o formulário', () => {
+    cy.get('#firstName').type('Gabriel')
+    cy.get('#lastName').type('Marques')
+    cy.get('#email').type('teste@teste.com')
+    cy.get('#open-text-area').type('Obrigado')
+    cy.get('.button[type="submit"]').click()
+
+    cy.get('.success').should('be.visible')
   })
 })
