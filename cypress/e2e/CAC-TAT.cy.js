cd7f50796b57910907c1ae64021d73502014a5a6
Gabriel Marques <bielmarques922@gmail.com>
2025-08-19 18:24:51 -0300

    🧪 test: Criando teste do Exercicio 2 Extra

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -17,7 +17,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('.success').should('be.visible')
   })
 
-  it.only('preenche os campos obrigatórios e envia o formulário - Exercicio extra 1', () => {
+  it('preenche os campos obrigatórios e envia o formulário - Exercicio extra 1', () => {
     const longText = Cypress._.repeat('abcdefghijklmnopqrstuvwxyz', 10)
     cy.get('#firstName').type('Gabriel')
     cy.get('#lastName').type('Marques')
@@ -26,5 +26,15 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('.button[type="submit"]').click()
 
     cy.get('.success').should('be.visible')
+  })
+
+  it.only('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida - Exercicio extra 2', () => {
+    cy.get('#firstName').type('Gabriel')
+    cy.get('#lastName').type('Marques')
+    cy.get('#email').type('teste@teste,com')
+    cy.get('#open-text-area').type('Teste')
+    cy.get('.button[type="submit"]').click()
+
+    cy.get('.error').should('be.visible')
   })
 })
